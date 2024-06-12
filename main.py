93998858b5daa175f1124524d3ce3aed07404916
Florian Schwanz <florian.schwanz@interoberlin.de>
2024-06-12 23:37:46 +0200

    chore: add data loader #1

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os
 import sys
 
 from lib.extract.data_extractor import extract_data
+from lib.load.data_loader import load_data
 from lib.tracking_decorator import TrackingDecorator
 from lib.transform.data_bounding_box_converter import convert_bounding_box
-from lib.transform.data_copier import copy_data
 from lib.transform.data_geometry_cleaner import clean_data_geometry
 from lib.transform.data_property_cleaner import clean_data_properties
 from lib.transform.data_property_extender import extend_data_properties
+from lib.transform.data_copier import copy_data
 
 file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
@@ -58,6 +59,12 @@ def main(argv):
     clean_data_geometry(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
     extend_data_properties(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
     convert_bounding_box(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
+
+    #
+    # Load
+    #
+
+    load_data(source_path=workspace_path, results_path=data_path, clean=clean, quiet=quiet)
 
 
 if __name__ == "__main__":
